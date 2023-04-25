cce1f9c6fd9a077b6f3712f9c8303cc980e65af7
GoldilocksKang <ceo5584@gmail.com>
2023-04-25 09:47:46 +0900

    CPP Class 추가

---
+++ Source/CPP0425/CPP0425.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CPP0425.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP0425, "CPP0425" );
